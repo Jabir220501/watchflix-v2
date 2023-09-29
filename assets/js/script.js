7fa8c5f1fc531a97856b93faf5bb1b96bd1ecb83
Jabir <220501@student.glu.nl>
2023-09-29 10:58:35 +0200

    cards in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,6 +19,12 @@ const getGenre = async (genreID) => {
   }
 };
 
+const roundToOneDecimalPlace = (number) => {
+  const roundedNumber = Math.round(number * 10) / 10;
+
+  return roundedNumber;
+};
+
 const justReleaseList = async () => {
   try {
     const response = await fetch(`${baseURL}/movie/upcoming`);
@@ -35,13 +41,19 @@ const justReleaseList = async () => {
       div.classList.add("row-slider-card", "vertical-large-card");
       div.innerHTML = `
           <div class="img-hover__container">
-            <a href="movie?id=${movieCard.id}"><img src="${image_url}${movieCard.backdrop_path}" alt="" loading="lazy"></a>
+            <a href="movie?id=${movieCard.id}"><img src="${image_url}${
+        movieCard.poster_path
+      }" alt="" loading="lazy"></a>
           </div>
           <div class="slider-card-info">
             <div class="slider-card-info__wrapper">
               <h3>${movieCard.original_title}</h3>
               <div class="slider-card-rate-and-genre">
-                <span><i class="fa-solid fa-star"></i> ${movieCard.vote_average} <span class="info_genre">| ${genreName || 'N/A'} • <span class="info_type-of-film">Movie</span></span></span>
+                <span><i class="fa-solid fa-star"></i> ${roundToOneDecimalPlace(
+                  movieCard.vote_average
+                )} <span class="info_genre">| ${
+        genreName || "N/A"
+      } • <span class="info_type-of-film">Movie</span></span></span>
               </div>
             </div>
           </div>
@@ -58,39 +70,125 @@ justReleaseList();
 
 const popularOfTheWeekList = async () => {
   try {
-    const response = await fetch(`${baseURL}/movie/popular`);
+    const response = await fetch(`${baseURL}/trending/movie`);
     const data = await response.json();
+    console.log(data);
     const justRelease = document.querySelector(
       ".popular-of-the-week-slider__container"
     );
     const image_url = "https://image.tmdb.org/t/p/original/";
 
+    let increaseNumber = 1;
     for (const movieCard of data.results) {
       const genreName = await getGenre(movieCard.genre_ids[0]);
-
       const div = document.createElement("div");
       div.classList.add("row-slider-card", "vertical-small-card");
       div.innerHTML = `
-      <div class="img-hover__container">
-      <a href="#"><img src="https://image.tmdb.org/t/p/original//8YFL5QQVPy3AgrEQxNYVSgiPEbe.jpg"
-              alt=""></a>
-  </div>
-  <div class="slider-card-info">
-      <div class="slider-card-info__wrapper">
-          <h3>Ant-Man and the Wasp: Quantumania</h3>
+      <div class="card-number-order">
+          <h4>${increaseNumber++}</h4>
       </div>
-  </div>
-  <div class="slider-card-rate-and-genre">
-      <span><i class="fa-solid fa-star"></i> 4.8 <span class="info_genre">| Action • <span
-                  class="info_type-of-film">Movie</span>
-          </span></span>
-  </div>
-        `;
+      <div class="img-hover__container">
+          <a href="${
+            movieCard.media_type == "movie" ? "movie" : "tv-show"
+          }?id=${movieCard.id}"><img src="${image_url}${
+        movieCard.poster_path
+      }"alt=""></a>
+      </div>
+      <div class="slider-card-info">
+          <div class="slider-card-info__wrapper">
+              <h3>${movieCard.title ? movieCard.title : movieCard.name}</h3>
+              <span class="info_genre">Action • Thriller</span>
+              <div class="slider-card-rate-and-genre">
+                  <span><i class="fa-solid fa-star"></i> ${roundToOneDecimalPlace(
+                    movieCard.vote_average
+                  )} <span class="info_type-of-film">|
+                          ${movieCard.media_type}</span>
+                  </span></span>
+              </div>
+          </div>
+      </div>
+      `;
       justRelease.appendChild(div);
     }
   } catch (error) {
     console.error("Error", error);
   }
 };
-
 popularOfTheWeekList();
+
+const randomMovies = async () => {
+  try {
+    const response = await fetch(`${baseURL}/discover/movie`);
+    const data = await response.json();
+    const randomMovies = document.querySelector(".movies-slider__container");
+    const image_url = "https://image.tmdb.org/t/p/original/";
+
+    for (const movieCard of data.results) {
+      const genreName = await getGenre(movieCard.genre_ids[0]);
+      const div = document.createElement("div");
+      div.classList.add("row-slider-card", "horizontal-card");
+      div.innerHTML = `
+      <div class="img-hover__container">
+      <a href="movie?id=${movieCard.id}"><img src="${image_url}${
+        movieCard.backdrop_path
+      }"alt=""></a>
+  </div>
+  <div class="slider-card-info">
+      <div class="slider-card-info__wrapper">
+      <h3>${movieCard.original_title}</h3>
+      </div>
+  </div>
+  <div class="slider-card-rate-and-genre">
+      <span><i class="fa-solid fa-star"></i> ${roundToOneDecimalPlace(
+        movieCard.vote_average
+      )} <span class="info_genre">|  ${genreName || "N/A"} • <span
+                  class="info_type-of-film">Movie</span>
+          </span></span>
+  </div>
+      `;
+      randomMovies.appendChild(div);
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+randomMovies();
+
+const randomSeries = async () => {
+  try {
+    const response = await fetch(`${baseURL}/trending/tv`);
+    const data = await response.json();
+    console.log(data);
+    const randomSeries = document.querySelector(".series-slider__container");
+    const image_url = "https://image.tmdb.org/t/p/original/";
+
+    for (const serieCard of data.results) {
+      const genreName = await getGenre(serieCard.genre_ids[0]);
+      const div = document.createElement("div");
+      div.classList.add("row-slider-card", "horizontal-card");
+      div.innerHTML = `
+      <div class="img-hover__container">
+      <a href="tv-show?id=${serieCard.id}"><img src="${image_url}${
+        serieCard.backdrop_path
+      }"alt=""></a>
+  </div>
+  <div class="slider-card-info">
+      <div class="slider-card-info__wrapper">
+      <h3>${serieCard.name}</h3>
+      </div>
+  </div>
+  <div class="slider-card-rate-and-genre">
+      <span><i class="fa-solid fa-star"></i> ${roundToOneDecimalPlace(
+        serieCard.vote_average
+      )} <span class="info_genre">|  ${genreName || "N/A"} • <span
+                  class="info_type-of-film">${serieCard.media_type}</span>
+          </span></span>
+  </div>
+      `;
+      randomSeries.appendChild(div);
+    }
+  } catch (error) {
+    console.error("Error", error);
+  }
+};
+randomSeries();
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const apiKey = "a527eb9bc54324ce4f762ac2b6d36ae6";
 
 // Function to fetch movie data
 async function fetchMovieData(apiEndpoint, res) {
-  const apiUrl = `https://api.themoviedb.org/3${apiEndpoint}?api_key=${apiKey}`;
+  const apiUrl = `https://api.themoviedb.org/3${apiEndpoint}api_key=${apiKey}`;
   try {
     const response = await axios.get(apiUrl);
     res.json(response.data);
@@ -22,19 +22,31 @@ async function fetchMovieData(apiEndpoint, res) {
 
 // Genre
 app.get("/genre/movie/list", (req, res) => {
-  fetchMovieData("/genre/movie/list", res);
+  fetchMovieData("/genre/movie/list?", res);
 });
 
 // Just Release
 app.get("/movie/upcoming", (req, res) => {
-  fetchMovieData("/movie/upcoming", res);
+  fetchMovieData("/movie/upcoming?", res);
 });
 
 // Popular Movie
-app.get("/movie/popular", (req, res) => {
-  fetchMovieData("/movie/popular", res);
+app.get("/trending/movie", (req, res) => {
+  fetchMovieData("/trending/all/week?", res);
+});
+
+// Random Movies
+app.get("/discover/movie", (req, res) => {
+  fetchMovieData("/discover/movie?sort_by=popularity.desc&", res);
+});
+
+// Random Series
+app.get("/trending/tv", (req, res) => {
+  fetchMovieData("/trending/tv/week?", res);
 });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+
